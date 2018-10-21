beb9737548885adaf1f8b398a3bd8b16a39ec8ac
codespectar <44140141+codespectar@users.noreply.github.com>
2018-10-21 22:50:27 +0200

    d

--- Domaci3.cs
+++
@@ -1,4 +0,0 @@
-/*Generator slučajnih brojeva bira n brojeva iz opsega 1 do 39. Brojeve treba
-smestiti u niz. Prikazati formirani niz. Proveriti da li je generisan broj 1 i u kom pokušaju. Ako
-je broj 1 generisan više puta, odrediti poziciju njegovog prvog i poziciju zadnjeg pojavljivanja.
-*/
