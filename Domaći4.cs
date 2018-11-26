91a278f09d2b46151c0dd87d952be265cb0dab50
codespectar <44140141+codespectar@users.noreply.github.com>
2018-11-26 22:01:53 +0100

    Domaći4

---
+++ Domaći4.cs
@@ -0,0 +1,6 @@
+/*Na osnovu zadatka koji se nalazi u fajlu TackaVektor.cs odraditi sledeće stavke:
+
+f. overload operatora - za određivanje razlike dva vektora (domaći)
+g. overload operatora * za određivanje skalarnog proizvoda dva vektora (domaći)
+h. overload operatora <, > za pore]enje dva vektora po intenzitetu (domaći) 
+*/
